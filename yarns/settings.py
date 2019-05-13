39c4f232931a0653851c9e04a57914a3bfbda209
tamina567 <tamina.pitt96@gmail.com>
2019-05-13 11:04:04 +1000

    Modularise navbar template for reuse.

--- yarns/settings.py
+++ yarns/settings.py
@@ -89,6 +89,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
